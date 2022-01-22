0a885cd22d51dc589642b0d3c16e3304a4027160
Chris Sinco <csinco.design@gmail.com>
2022-01-22 09:59:55 -0800

    Fix theme on main

--- app/src/main/java/com/example/shrinecompose/Cart.kt
+++ app/src/main/java/com/example/shrinecompose/Cart.kt
@@ -34,7 +34,7 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.shrinecompose.SampleData.Companion.SampleItems
-import com.example.shrinecompose.ui.theme.Theme.Companion.ShrineComposeTheme
+import com.example.shrinecompose.ui.theme.ShrineComposeTheme
 
 @Composable
 private fun CartHeader(cartSize: Int) {
