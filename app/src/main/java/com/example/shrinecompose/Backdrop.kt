d03698db02d8dd992f40c90bb7ad039b40ef8ea4
Chris Sinco <csinco@google.com>
2022-09-29 13:46:58 -0700

    Prep clean state

--- app/src/main/java/com/example/shrinecompose/Backdrop.kt
+++ app/src/main/java/com/example/shrinecompose/Backdrop.kt
@@ -16,32 +16,71 @@
 
 package com.example.shrinecompose
 
-import androidx.compose.animation.*
-import androidx.compose.animation.core.*
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.with
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.*
+import androidx.compose.material.BackdropScaffold
+import androidx.compose.material.BackdropValue
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.rememberBackdropScaffoldState
 import androidx.compose.material.ripple.rememberRipple
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.shrinecompose.ui.theme.ShrineComposeTheme
-import com.example.shrinecompose.ui.theme.ShrineScrimColor
 import kotlinx.coroutines.launch
 
 @Composable
@@ -327,8 +366,6 @@ fun NavigationMenuPreview() {
 @ExperimentalMaterialApi
 @Composable
 fun Backdrop(
-    showScrim: Boolean = false,
-    onAddCartItem: (FirstAddCartItemData) -> Unit = {},
     onBackdropReveal: (Boolean) -> Unit = {}
 ) {
     val scaffoldState = rememberBackdropScaffoldState(BackdropValue.Concealed)
@@ -358,24 +395,16 @@ fun Backdrop(
             )
         },
         frontLayerContent = {
-            val scrimAlpha by animateFloatAsState(
-                targetValue = if (showScrim) 0.6f else 0f,
-                animationSpec = tween(durationMillis = 500, easing = LinearEasing)
-            )
-            Catalog(
-                modifier = Modifier.drawWithContent {
-                    drawContent()
-                    drawRect(color = ShrineScrimColor, alpha = scrimAlpha)
-                },
-                items = SampleItems.filter {
-                    activeCategory == Category.All || it.category == activeCategory
-                },
-                onAddCartItem = onAddCartItem
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(20.dp)
+            ) {
+                Text("This is the content for category: $activeCategory")
+            }
         },
         frontLayerShape = MaterialTheme.shapes.large,
         frontLayerElevation = 16.dp,
-        frontLayerScrimColor = ShrineScrimColor.copy(alpha = 0.6f),
         backLayerContent = {
             NavigationMenu(
                 modifier = Modifier.padding(top = 12.dp, bottom = 32.dp),
--- app/src/main/java/com/example/shrinecompose/ShrineApp.kt
+++ app/src/main/java/com/example/shrinecompose/ShrineApp.kt
@@ -4,8 +4,13 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
@@ -15,17 +20,11 @@ import androidx.compose.ui.Modifier
 fun ShrineApp() {
     var sheetState by rememberSaveable { mutableStateOf(CartBottomSheetState.Collapsed) }
     val cartItems = remember { mutableStateListOf(*SampleItems.take(2).toTypedArray()) }
-    var firstAddCartItem by remember { mutableStateOf<FirstAddCartItemData?>(null) }
 
     BoxWithConstraints(
         Modifier.fillMaxSize()
     ) {
         Backdrop(
-            showScrim = sheetState == CartBottomSheetState.Expanded,
-            onAddCartItem = {
-                if (cartItems.isEmpty()) firstAddCartItem = it
-                cartItems.add(it.data)
-            },
             onBackdropReveal = { revealed ->
                 sheetState = if (revealed) CartBottomSheetState.Hidden else CartBottomSheetState.Collapsed
             }
@@ -38,20 +37,11 @@ fun ShrineApp() {
             maxWidth = maxWidth,
             onRemoveItemFromCart = {
                 cartItems.remove(it)
-                if (cartItems.isEmpty()) firstAddCartItem = null
             },
             onSheetStateChange = {
                 sheetState = it
             }
         )
-        if (firstAddCartItem != null) {
-            FirstAddCartItem(
-                data = firstAddCartItem!!
-            ) {
-                // Temporary to dismiss
-                firstAddCartItem = null
-            }
-        }
     }
 }
 
